94021861ceb71ea61a1fa905ace3f728a135e9ec
Adrien Nhem <adriennhem@users.noreply.github.com>
2023-11-07 16:46:09 +0000

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.active_record.sqlite3_production_warning=false
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
